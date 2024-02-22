3788c92089d11d147fa6ec7a746319589ed175bb
marcon21 <marcon859@gmail.com>
2024-02-22 18:55:05 +0100

    Implemented all voting systems, added utilities for reading files, added test

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,22 @@
 import json
 import numpy as np
+import pandas as pd
 
-def read_voting(file_path: str) -> np.array:
+
+class VotingArray(np.ndarray):
+    def to_pandas(self):
+        p, v = self.shape
+        voting_table = pd.DataFrame(
+            self,
+            columns=[f"voter_{i}" for i in range(v)],
+            index=[f"preference_{i}" for i in range(p)],
+        )
+        return voting_table
+
+
+def read_voting(file_path: str, table_name: str = "voting") -> VotingArray:
     with open(file_path) as f:
-        voting = json.load(f)["voting"]
-        voting = np.array(voting)
-        return voting
+        voting = json.load(f)[table_name]
+        voting = np.array(voting).view(VotingArray)
+
+    return voting
