fe9656a8fc98402c812fc75a0e516379d669e6e0
MarioRealEn <mario.real.en@gmail.com>
2024-02-26 11:55:22 +0100

    Added hapiness_level and strategic_voting_risk

--- src/happiness_level.py
+++ src/happiness_level.py
@@ -1,5 +1,21 @@
 import numpy as np
+import math
 
-def happiness_level(s: np.array) -> float:
-    return .0
+def happiness_level(pref: np.array, outcome: str) -> np.array:
+    m = pref.shape[0]
+    n = pref.shape[1]
+    new_voting = np.array([[pref[i][j] for i in range(m)] for j in range(n)])
+    h = list()
+    for ivoter in range(n):
+        d = np.where(new_voting[ivoter]==outcome)[0][0]
+        h.append(distr_h(d, m))
+    return h
+
+def distr_h(d: float, m: int) -> float:
+    h_i = (1-2/(m-1)*d)
+    k = 0.95
+    c = 1/math.atanh(k)
+    h = math.atanh(h_i*k)*c
+    return h
+
 
