74b58dac32e3d7a35ff58e25eb45258771b09dfe
tarcisio16 <160622023+tarcisio16@users.noreply.github.com>
2024-03-19 14:14:49 +0100

    added functions for calculating happiness

--- src/optimized_code.py
+++ src/optimized_code.py
@@ -3,6 +3,35 @@ import math
 from typing import Callable
 from itertools import permutations
 from collections import Counter
+from joblib import Parallel, delayed
+from typing import Callable, List
+
+def calculate_voter_risk_and_happinesses(preferences, result, initial_happiness, i, schema_outcome_f):
+    vwr_index = np.argmax(preferences[:, i] == result)
+    best_happiness = initial_happiness[i]
+    increasing_permutations = []
+    for p in permutations(preferences[:, i]):
+        p = np.array(p)
+        p_winner_index = np.argmax(p == result)
+        if p_winner_index >= vwr_index:
+            new_result = schema_outcome_f(preferences[:, i] if p_winner_index == vwr_index else p)
+            new_happiness = happiness_level(preferences if p_winner_index == vwr_index else np.column_stack((preferences[:, :i], p, preferences[:, i+1:])), i, new_result)
+            if new_happiness > best_happiness:
+                best_happiness = new_happiness
+                increasing_permutations = [p]
+            elif new_happiness == best_happiness:
+                increasing_permutations.append(p)
+    return best_happiness - initial_happiness[i], increasing_permutations
+
+def calculate_risk_and_happinesses(preferences: np.ndarray, schema_outcome_f: Callable) -> (float, List[np.ndarray]):
+    result = schema_outcome_f(preferences)
+    initial_happiness = np.array([happiness_level(preferences, voter, result) for voter in range(preferences.shape[1])])
+    total_risk, all_increasing_permutations = zip(*Parallel(n_jobs=-1)(
+        delayed(calculate_voter_risk_and_happinesses)(preferences, result, initial_happiness, i, schema_outcome_f) for i in range(preferences.shape[1])))
+    num_unhappy_voters = np.count_nonzero(initial_happiness != 1)
+    avg_risk = sum(total_risk) / num_unhappy_voters if num_unhappy_voters != 0 else 0
+    return avg_risk, [perm for perm_list in all_increasing_permutations for perm in perm_list]
+
 
 def generate_random_voting_situation_np(num_voters: int, num_candidates: int) -> np.ndarray:
     return np.array([np.random.permutation(np.arange(num_candidates)) for _ in range(num_voters)]).T
