fa739ad5d1d8051f36b60d6cf157ecd076519d80
tarcisio16 <160622023+tarcisio16@users.noreply.github.com>
2024-03-26 16:13:33 +0100

    MODIFY

--- src/optimized_code.py
+++ src/optimized_code.py
@@ -4,9 +4,24 @@ import math, json
 from typing import Callable, Tuple, List
 from itertools import permutations
 from collections import Counter
-from joblib import Parallel, delayed
 import json 
-from joblib import Parallel, delayed
+
+    
+def get_scheme(voting_scheme:np.ndarray)-> Callable:
+    
+    if all(voting_scheme[i] == len(voting_scheme) - i for i in range(len(voting_scheme))):
+        return borda_outcome_op
+    if voting_scheme[0] == 1 and voting_scheme[1] == 0:
+        return plurality_outcome_op
+        
+    if voting_scheme[0] == 1 and voting_scheme[1] == 1  and voting_scheme[2] == 0:
+        return for_two_outcome_op
+        
+    if all(voting_scheme[i] == 1 for i in range(len(voting_scheme) - 1)) and voting_scheme[-1] == 0:
+        return veto_outcome_op
+        
+    raise ValueError("Invalid voting scheme")
+
 
 def read_voting_(file_path: str, table_name: str = "voting"):
     with open(file_path) as f:
@@ -25,69 +40,52 @@ def for_two_outcome_op(arr: np.ndarray) -> int:
     max_elements = [key for key, value in counts.items() if value == max_count]
     return min(max_elements, default=None)
 
-def veto_outcome_op(arr: np.ndarray) -> int:
-    unique_values, counts = np.unique(arr[:-1].ravel(), return_counts=True)
-    if len(counts) == 0:
-        return None
-    max_count = np.max(counts)
-    max_elements = unique_values[counts == max_count]
-    return np.min(max_elements) if len(max_elements) > 0 else None
+
+def veto_outcome_op(arr: np.ndarray):
+    counts = Counter(arr[:-1].ravel()) 
+    max_count = max(counts.values())
+    max_elements = [key for key, value in counts.items() if value == max_count]
+    return min(max_elements)
 
 def borda_outcome_op(preferences: np.ndarray) -> int:
     n, m = preferences.shape
     alternatives, counts = np.unique(preferences, return_counts=True)
-
-    # Initialize Borda points using vectorized operations
     borda_points = np.zeros(len(alternatives))
     for i, alternative in enumerate(alternatives):
         borda_points[i] = ((n - 1) * counts[i] - (np.where(preferences == alternative)[0] * (n - 1 - np.arange(n)).reshape(-1, 1)).sum())
-
-    # Find the index of the maximum Borda points
     winner_index = np.argmax(borda_points)
-
-    # Return the alternative corresponding to the winner index
     winner = alternatives[winner_index]
     return winner
 
-def happiness_level(preferences: np.ndarray,num_voter: int, outcome: int) -> float:
-    vwr = np.argmax(preferences[:, num_voter] == outcome)
+def happiness_level(vwr, candidates) -> float:
     k = 0.95
     c = 1 / (2 * math.atanh(k))
-    h_i = 1 - 2 / (preferences.shape[0] - 1) * vwr
+    h_i = 1 - 2 / (candidates - 1) * vwr
     h = math.atanh(h_i * k) * c + 0.5
     return h
 
-def happiness_level_total(preferences: np.ndarray, outcome: int) -> float:
-    hap = np.zeros(preferences.shape[1])
-    for voter in range(preferences.shape[1]):
-        vwr = np.argmax(preferences[:, voter] == outcome)
-        k = 0.95
-        c = 1 / (2 * math.atanh(k))
-        h_i = 1 - 2 / (preferences.shape[0] - 1) * vwr
-        hap[voter] = math.atanh(h_i * k) * c + 0.5
-    return hap
-
-
+def happiness_level_total(preferences: np.ndarray, outcome: int) -> np.ndarray:
+    num_voters = preferences.shape[1]
+    happiness_levels = np.zeros(num_voters)
+    candidates = preferences.shape[0]
+    for voter in range(num_voters):
+        vwr = np.argwhere(preferences[:, voter] == outcome)[0][0]
+        happiness_levels[voter] = happiness_level(vwr, candidates)
+    return happiness_levels
 
 def compute_voter_risk(preferences: np.ndarray, result: int, initial_happinesses: np.ndarray, i: int, schema_outcome_f: Callable) -> float:
     initial_happiness = initial_happinesses[i]
-    vwr_idx = np.flatnonzero(preferences[:, i] == result)
-    if vwr_idx.size == 0:
-        return 0 
-
-    vwr = vwr_idx[0]
+    candidates = preferences.shape[0]
+    vwr = np.argwhere(preferences[:, i] == result)[0][0]
     best_happiness = initial_happiness
 
-    perms = list(permutations(preferences[:, i]))
-    perm_array = np.array(perms)
-
-    new_voting = preferences.copy()
-    for perm in perm_array:
-        idx = np.argwhere(perm == result)[0][0]
-        if idx >= vwr:
+    for perm in permutations(preferences[:, i]):
+        if np.argwhere(perm == result)[0][0] >= vwr:
+            new_voting = preferences.copy()
             new_voting[:, i] = perm
             new_result = schema_outcome_f(new_voting)
-            new_happiness = happiness_level(preferences, i, new_result)
+            new_vwr = np.argwhere(preferences[:, i] == new_result)[0][0]
+            new_happiness = happiness_level(new_vwr, candidates)
             best_happiness = max(best_happiness, new_happiness)
 
     return best_happiness - initial_happiness
@@ -104,42 +102,6 @@ def compute_risk(preferences: np.ndarray, schema_outcome_f: Callable) -> float:
         return 0
     return total_risk / num_unhappy_voters
 
-def compute_voter_risk_parallel(preferences: np.ndarray, result: int, initial_happinesses: np.ndarray, i: int, schema_outcome_f: Callable) -> float:
-    initial_happiness = initial_happinesses[i]
-    vwr_idx = np.flatnonzero(preferences[:, i] == result)
-    if vwr_idx.size == 0:
-        return 0
-
-    vwr = vwr_idx[0]
-    best_happiness = initial_happiness
-
-    perms = list(permutations(preferences[:, i]))
-    perm_array = np.array(perms)
-
-    new_voting = preferences.copy()
-    for perm in perm_array:
-        idx = np.argwhere(perm == result)[0][0]
-        if idx >= vwr:
-            new_voting[:, i] = perm
-            new_result = schema_outcome_f(new_voting)
-            new_happiness = happiness_level(preferences, i, new_result)
-            best_happiness = max(best_happiness, new_happiness)
-
-    return best_happiness - initial_happiness
-
-def compute_risk_parallel(preferences: np.ndarray, schema_outcome_f: Callable,cores: int) -> float:
-    result = schema_outcome_f(preferences)
-    initial_happinesses = happiness_level_total(preferences, result)
-    num_unhappy_voters = np.count_nonzero(initial_happinesses != 1)
-
-    if num_unhappy_voters == 0:
-        return 0
-
-    num_voters = preferences.shape[1]
-    risks = Parallel(n_jobs=cores)(delayed(compute_voter_risk_parallel)(preferences, result, initial_happinesses, voter, schema_outcome_f) for voter in range(num_voters))
-    total_risk = sum(risks)
-
-    return total_risk / num_unhappy_voters
 
 def compute_voter_risk_combinations(preferences: np.ndarray, 
                                     result: int, 
@@ -148,30 +110,22 @@ def compute_voter_risk_combinations(preferences: np.ndarray,
                                     schema_outcome_f: Callable,
                                     strategies_happiness: pd.DataFrame) -> float:
     initial_happiness = initial_happinesses[i]
-    vwr_idx = np.flatnonzero(preferences[:, i] == result)
-    
-    if not vwr_idx.size:
-        return 0
-    
-    vwr = vwr_idx[0]
+    candidates = preferences.shape[0]
+    vwr = np.argwhere(preferences[:, i] == result)[0][0]
     best_happiness = initial_happiness
     
+    new_voting = preferences.copy()
     for perm in permutations(preferences[:, i]):
-        idx = np.where(perm == result)[0][0]
-        
-        if idx >= vwr:
-            new_voting = preferences.copy()
+        if np.argwhere(perm == result)[0][0] >= vwr:
             new_voting[:, i] = perm
             new_result = schema_outcome_f(new_voting)
-            new_happiness = happiness_level(preferences, i, new_result)
+            new_vwr = np.argwhere(preferences[:, i] == new_result)[0][0]
+            new_happiness = happiness_level(new_vwr, candidates)
             
             if new_happiness > initial_happiness:
-                strategies_happiness.loc[len(strategies_happiness)] = [i, perm, new_result, new_happiness, initial_happiness, happiness_level_total(preferences,new_result).sum()]  # Skip unnecessary computation of new_overall_happiness
+                strategies_happiness.loc[len(strategies_happiness)] = [i, perm, new_result, new_happiness, initial_happiness, sum(happiness_level_total(preferences,new_result))] 
                 
-            if new_happiness == 1:
-                return 1 - initial_happiness
-            
-            best_happiness = max(best_happiness, new_happiness)
+            best_happiness =  max(new_happiness,best_happiness)
     
     return best_happiness - initial_happiness
 
@@ -182,7 +136,7 @@ def compute_risk_combinations(preferences: np.ndarray,
     
     result = schema_outcome_f(preferences)
     initial_happinesses = happiness_level_total(preferences, result)
-    initial_overall_happiness = initial_happinesses.sum()
+    initial_overall_happiness = sum(initial_happinesses)
     num_unhappy_voters = np.count_nonzero(initial_happinesses != 1)
     strategies_happiness = pd.DataFrame(columns=["voter", "combination", "new_result", "strategic_happiness", "old_happiness", "overall_happiness"])
 
